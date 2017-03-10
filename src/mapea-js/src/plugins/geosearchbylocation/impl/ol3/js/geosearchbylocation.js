dba9efb06a6b4b39a448cdca51cde25612155d08
Manuel Outeiriño <manuelouteirino@guadaltel.com>
2017-03-10 11:53:37 +0100

    se eliminan los directorios /ol3/ de los plugins (se renombraron a /ol/)

--- src/mapea-js/src/plugins/geosearchbylocation/impl/ol3/js/geosearchbylocation.js
+++
@@ -1,7 +0,0 @@
-goog.provide('P.impl.plugin.Geosearchbylocation');
-
-goog.require('P.impl.control.Geosearchbylocation');
-goog.require('P.plugin.Geosearchbylocation');
-
-// this file it is just used in order to imports
-
