42463e87eb3f75e228962b26af42687314424d90
manmorjim <manmorjim@gmail.com>
2017-05-03 14:42:00 +0200

    Cambios para implementar el método refresh del mapa

--- mapea-js/src/impl/ol/js/layers/wms.js
+++ mapea-js/src/impl/ol/js/layers/wms.js
@@ -5,13 +5,14 @@ goog.require('M.exception');
 goog.require('M.impl.Layer');
 goog.require('M.impl.format.WMSCapabilities');
 goog.require('M.impl.GetCapabilities');
+goog.require('M.impl.source.TileWMS');
+goog.require('M.impl.source.ImageWMS');
 
 goog.require('ol.layer.Tile');
 goog.require('ol.layer.Image');
-goog.require('ol.source.TileWMS');
 goog.require('ol.tilegrid.TileGrid');
-goog.require('ol.source.ImageWMS');
 goog.require('ol.extent');
+
 
 (function () {
   /**
@@ -239,7 +240,7 @@ goog.require('ol.extent');
 
         var newSource;
         if (this_.tiled === true) {
-          newSource = new ol.source.TileWMS({
+          newSource = new M.impl.source.TileWMS({
             url: this_.url,
             params: layerParams,
             tileGrid: new ol.tilegrid.TileGrid({
@@ -255,7 +256,7 @@ goog.require('ol.extent');
           });
         }
         else {
-          newSource = new ol.source.ImageWMS({
+          newSource = new M.impl.source.ImageWMS({
             url: this_.url,
             params: layerParams,
             resolutions: resolutions
@@ -318,7 +319,7 @@ goog.require('ol.extent');
       if (this.tiled === true) {
         this.ol3Layer = new ol.layer.Tile({
           visible: this.visibility && (this.options.visibility !== false),
-          source: new ol.source.TileWMS({
+          source: new M.impl.source.TileWMS({
             url: this.url,
             params: layerParams,
             tileGrid: tileGrid
@@ -333,7 +334,7 @@ goog.require('ol.extent');
       else {
         this.ol3Layer = new ol.layer.Image({
           visible: this.visibility && (this.options.visibility !== false),
-          source: new ol.source.ImageWMS({
+          source: new M.impl.source.ImageWMS({
             url: this.url,
             params: layerParams
           }),
@@ -578,6 +579,22 @@ goog.require('ol.extent');
    */
   M.impl.layer.WMS.prototype.setLegendURL = function (legendUrl) {
     this.legendUrl_ = legendUrl;
+  };
+
+  /**
+   * This function refreshes the state of this
+   * layer
+   *
+   * @public
+   * @function
+   * @api stable
+   * @export
+   */
+  M.impl.layer.WMS.prototype.refresh = function () {
+    var ol3Layer = this.getOL3Layer();
+    if (!M.utils.isNullOrEmpty(ol3Layer)) {
+      ol3Layer.getSource().changed();
+    }
   };
 
   /**
