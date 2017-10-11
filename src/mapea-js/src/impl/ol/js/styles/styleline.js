a90fd012230c1001ed283d8a58f87e7db7dbb5f1
carmenmarquez <carmenmarquez@guadaltel.com>
2017-10-11 18:10:53 +0200

    #90454: smooth

--- src/mapea-js/src/impl/ol/js/styles/styleline.js
+++ src/mapea-js/src/impl/ol/js/styles/styleline.js
@@ -39,7 +39,6 @@ goog.require('M.impl.style.TextPath');
       let stroke = options.stroke;
       let label = options.label;
       let fill = options.fill;
-      let geometry = options.geometry;
       let style = new M.impl.style.CentroidStyle();
       let styleStroke = new M.impl.style.CentroidStyle();
       const getValue = M.impl.style.Simple.getValue;
