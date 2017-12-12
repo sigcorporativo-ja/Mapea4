6ac8c550d54b85188a2ac541888a141463510998
emrivero <emiliomartinez@guadaltel.com>
2017-12-12 12:13:06 +0100

    Se añaden requires de styleheatmap a map.js

--- src/mapea-js/src/facade/js/map/map.js
+++ src/mapea-js/src/facade/js/map/map.js
@@ -46,6 +46,8 @@ goog.require('M.style.Cluster');
 goog.require('M.style.Choropleth');
 goog.require('M.style.Category');
 goog.require('M.style.Proportional');
+goog.require('M.style.Heatmap');
+
 (function() {
   /**
    * @classdesc
