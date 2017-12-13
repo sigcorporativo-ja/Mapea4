1ed1ec72edec6eac4fe0ce62abac151c56585acd
Manuel Jesús Morillo Jiménez <manueljmorillo@guadaltel.com>
2017-12-13 16:50:54 +0100

    Se corrige el error de cargar las imágenes de los features englobados por un cluster cuando se despliegan por primera vez

--- mapea-js/src/impl/ol/js/interactions/selectedclusterinteraction.js
+++ mapea-js/src/impl/ol/js/interactions/selectedclusterinteraction.js
@@ -249,22 +249,6 @@ M.impl.interaction.SelectCluster.prototype.drawAnimatedFeatureAndLink_ = functio
   }
   let olClusterStyles = clusterStyleFn(clusterFeature, resolution);
   let clonedStyles = olClusterStyles.map ? olClusterStyles.map(s => s.clone()) : [olClusterStyles.clone()];
-  if (!M.utils.isNullOrEmpty(clonedStyles)) {
-    clonedStyles.forEach(function(olStyle) {
-      let styleImage = olStyle.getImage();
-      if (!M.utils.isNullOrEmpty(styleImage)) {
-        if (styleImage.getOrigin() == null) {
-          styleImage.origin_ = [];
-        }
-        if (styleImage.getAnchor() == null) {
-          styleImage.normalizedAnchor_ = [];
-        }
-        if (styleImage.getSize() == null) {
-          styleImage.size_ = [42, 42];
-        }
-      }
-    });
-  }
 
   cf.setId(clusterFeature.getId());
   cf.setStyle(clonedStyles);
