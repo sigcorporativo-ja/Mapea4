df5673a57da04f0af35b754f64ed5c824ee4d4c5
emrivero <emiliomartinez@guadaltel.com>
2018-02-22 13:19:03 +0100

    Tarea #98292 Incidencia #98252
    Se añade parametro crs al constructor de M.layer.GeoJSON
    Se añade método setSource a la clase M.layer.GeoJSON

--- src/mapea-js/src/facade/js/layers/geojson.js
+++ src/mapea-js/src/facade/js/layers/geojson.js
@@ -55,6 +55,24 @@ goog.require('M.exception');
 
       // extract
       this.extract = parameters.extract;
+      //crs
+      if (!M.utils.isNullOrEmpty(parameters.crs)) {
+
+        if (M.utils.isNullOrEmpty(this.source)) {
+          this.source = {
+            "type": "FeatureCollection",
+            "features": []
+          };
+        }
+
+        this.source['crs'] = {
+          "type": "EPSG",
+          "properties": {
+            "code": parameters.crs
+          }
+        };
+
+      }
     }
 
     if (M.utils.isNullOrEmpty(this.extract)) {
@@ -137,6 +155,17 @@ goog.require('M.exception');
     return equals;
   };
 
+  /**
+   * This function checks if an object is equals
+   * to this layer
+   *
+   * @function
+   * @api stable
+   */
+  M.layer.GeoJSON.prototype.setSource = function(source) {
+    this.source = source;
+  };
+
   M.layer.GeoJSON.prototype.setStyle = function(style, applyToFeature = false) {
     let isNullStyle = false;
     if (style === null) {
@@ -167,7 +196,7 @@ goog.require('M.exception');
       applyStyleFn.bind(this)();
     }
     else {
-      this.on(M.evt.LOAD, applyStyleFn, this);
+      this.once(M.evt.LOAD, applyStyleFn, this);
     }
   };
 
