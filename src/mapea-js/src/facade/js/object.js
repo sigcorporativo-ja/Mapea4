a9e3f02777096e1d8f0c75d3822b8fc8c2d5b62b
carloscastellano <carloscastellano@carloscastellano.guadaltel.es>
2018-06-22 12:48:49 +0200

    Tarea #106281 Migración completada de la clase facade/object.js ES6

--- src/mapea-js/src/facade/js/object.js
+++ src/mapea-js/src/facade/js/object.js
@@ -1,85 +1,81 @@
-goog.provide('M.Object');
+import EventsManager from('./eventsmanager.js');
+import EvtListener from('./eventsmanager.js');
+import Utils from('./utils/utils.js');
 
-goog.require('M.evt.EventsManager');
-goog.require('M.evt.Listener');
+/**
+ * @classdesc
+ * Main mapea Object. This class creates a Object
+ * which manages events
+ *
+ * @constructor
+ * @api stable
+ */
+class Object {
 
-(function () {
-   'use strict';
-   /**
-    * @classdesc
-    * Main mapea Object. This class creates a Object
-    * which manages events
-    *
-    * @constructor
-    * @api stable
-    */
-   M.Object = (function () {
+  /**
+   * Callback for events managed by the
+   * facade object
+   *
+   * @private
+   * @type {M.evt.EventsManager}
+   */
+  this.eventsManager = new EvtListener();
+});
 
-      /**
-       * Callback for events managed by the
-       * facade object
-       *
-       * @private
-       * @type {M.evt.EventsManager}
-       */
-      this.eventsManager_ = new M.evt.EventsManager();
-   });
+/**
+ * Sets the callback when the instace is loaded
+ *
+ * @public
+ * @function
+ * @api stable
+ */
+on(eventType, listener, optThis) {
+  return this.eventsManager.add(eventType, listener, optThis);
+};
 
-   /**
-    * Sets the callback when the instace is loaded
-    *
-    * @public
-    * @function
-    * @api stable
-    */
-   M.Object.prototype.on = function (eventType, listener, optThis) {
-      return this.eventsManager_.add(eventType, listener, optThis);
-   };
+/**
+ * Sets the callback when the instace is loaded
+ *
+ * @public
+ * @function
+ * @api stable
+ */
+once(eventType, listener, optThis) {
+  return this.eventsManager.add(eventType, listener, optThis, true);
+};
 
-   /**
-    * Sets the callback when the instace is loaded
-    *
-    * @public
-    * @function
-    * @api stable
-    */
-   M.Object.prototype.once = function (eventType, listener, optThis) {
-      return this.eventsManager_.add(eventType, listener, optThis, true);
-   };
+/**
+ * Sets the callback when the instace is loaded
+ *
+ * @public
+ * @function
+ * @api stable
+ */
+once(eventType, listener, optThis) {
+  return this.eventsManager.add(eventType, listener, optThis, true);
+};
 
-     /**
-      * Sets the callback when the instace is loaded
-      *
-      * @public
-      * @function
-      * @api stable
-      */
-    M.Object.prototype.once = function (eventType, listener, optThis) {
-        return this.eventsManager_.add(eventType, listener, optThis, true);
-    };
+/**
+ * Sets the callback when the instace is loaded
+ *
+ * @public
+ * @function
+ * @api stable
+ */
+un(eventType, listener, optThis) {
+  this.eventsManager.remove(eventType, listener, optThis);
+};
 
-   /**
-    * Sets the callback when the instace is loaded
-    *
-    * @public
-    * @function
-    * @api stable
-    */
-   M.Object.prototype.un = function (eventType, listener, optThis) {
-      this.eventsManager_.remove(eventType, listener, optThis);
-   };
-
-   /**
-    * Sets the callback when the instace is loaded
-    *
-    * @public
-    * @function
-    * @api stable
-    */
-   M.Object.prototype.fire = function (eventType, args) {
-      if (M.utils.isNullOrEmpty(args)) {
-         args = [this];
-      }
-      this.eventsManager_.fire(eventType, args);
-   };
-})();
+/**
+ * Sets the callback when the instace is loaded
+ *
+ * @public
+ * @function
+ * @api stable
+ */
+fire(eventType, args) {
+  if (Utils.isNullOrEmpty(args)) {
+    args = [this];
+  }
+  this.eventsManager.fire(eventType, args);
+};
