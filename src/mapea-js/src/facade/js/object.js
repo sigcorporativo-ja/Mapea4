9b5ac25aeb37c8964ec2553c66f4149546c1ebb0
emrivero <emiliomartinez@guadaltel.com>
2018-07-04 13:32:39 +0200

    Peerreview #107042 #106281
    Se suben las correcciones vistas en la peerreview a la clase
    facade/object 20180704.

--- src/mapea-js/src/facade/js/object.js
+++ src/mapea-js/src/facade/js/object.js
@@ -1,6 +1,5 @@
-import EventsManager from './eventsmanager.js';
-import EvtListener from './eventsmanager.js';
-import Utils from './utils/utils.js';
+import EventsManager from './event/eventsmanager';
+import Utils from './utils/utils';
 
 /**
  * @classdesc
@@ -12,75 +11,72 @@ import Utils from './utils/utils.js';
  */
 export default class Object {
 
-  /**
-   * Callback for events managed by the
-   * facade object
-   *
-   * @private
-   * @type {M.evt.EventsManager}
-   */
-  this.eventsManager_ = new EvtListener();
-});
-
-// TODO
-getEventsManager() {
-  return this.eventsManager_;
-}
-
-/**
- * Sets the callback when the instace is loaded
- *
- * @public
- * @function
- * @api stable
- */
-on(eventType, listener, optThis) {
-  return this.eventsManager.add(eventType, listener, optThis);
-}
-
-/**
- * Sets the callback when the instace is loaded
- *
- * @public
- * @function
- * @api stable
- */
-once(eventType, listener, optThis) {
-  return this.eventsManager.add(eventType, listener, optThis, true);
-}
-
-/**
- * Sets the callback when the instace is loaded
- *
- * @public
- * @function
- * @api stable
- */
-once(eventType, listener, optThis) {
-  return this.eventsManager.add(eventType, listener, optThis, true);
-}
-
-/**
- * Sets the callback when the instace is loaded
- *
- * @public
- * @function
- * @api stable
- */
-un(eventType, listener, optThis) {
-  this.eventsManager.remove(eventType, listener, optThis);
-}
-
-/**
- * Sets the callback when the instace is loaded
- *
- * @public
- * @function
- * @api stable
- */
-fire(eventType, args) {
-  if (Utils.isNullOrEmpty(args)) {
-    args = [this];
+  constructor() {
+    /**
+     * Callback for events managed by the
+     * facade object
+     *
+     * @private
+     * @type {M.evt.EventsManager}
+     */
+    this.eventsManager_ = new EventsManager();
   }
-  this.eventsManager.fire(eventType, args);
+
+  /**
+   * Sets the callback when the instace is loaded
+   *
+   * @public
+   * @function
+   * @api stable
+   */
+  on(eventType, listener, optThis) {
+    return this.eventsManager_.add(eventType, listener, optThis);
+  }
+
+  /**
+   * Sets the callback when the instace is loaded
+   *
+   * @public
+   * @function
+   * @api stable
+   */
+  once(eventType, listener, optThis) {
+    return this.eventsManager_.add(eventType, listener, optThis, true);
+  }
+
+  /**
+   * Sets the callback when the instace is loaded
+   *
+   * @public
+   * @function
+   * @api stable
+   */
+  once(eventType, listener, optThis) {
+    return this.eventsManager_.add(eventType, listener, optThis, true);
+  }
+
+  /**
+   * Sets the callback when the instace is loaded
+   *
+   * @public
+   * @function
+   * @api stable
+   */
+  un(eventType, listener, optThis) {
+    this.eventsManager_.remove(eventType, listener, optThis);
+  }
+
+  /**
+   * Sets the callback when the instace is loaded
+   *
+   * @public
+   * @function
+   * @api stable
+   */
+  fire(eventType, args) {
+    if (Utils.isNullOrEmpty(args)) {
+      args = [this];
+    }
+    this.eventsManager_.fire(eventType, args);
+  }
 }
