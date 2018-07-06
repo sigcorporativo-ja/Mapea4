a065d0dbe9382c676d12534b5d7d086705ae4d03
carloscastellano <carloscastellano@carloscastellano.guadaltel.es>
2018-07-06 11:51:01 +0200

    Tarea #107280 Primera subida del modulo impl/view de la migracion a modulos ES6.

--- src/mapea-js/src/impl/ol/js/view/view.js
+++ src/mapea-js/src/impl/ol/js/view/view.js
@@ -1,85 +1,81 @@
-goog.provide('M.impl.View');
+export default class View extends ol.View {
+  /**
+   * @classdesc
+   *
+   * @constructor
+   * @extends {ol.View}
+   * @param {olx.ViewOptions=} opt_options View options.
+   * @api stable
+   */
+  constructor(opt_options) {
+    super(opt_options);
 
-goog.require('ol.View');
+    this.userZoom_ = null;
 
-(function () {
-   /**
-    * @classdesc
-    *
-    * @constructor
-    * @extends {ol.View}
-    * @param {olx.ViewOptions=} opt_options View options.
-    * @api stable
-    */
-   M.impl.View = function (opt_options) {
-      this.userZoom_ = null;
-
-      goog.base(this, opt_options);
-   };
-   goog.inherits(M.impl.View, ol.View);
+  }
 
 
-   /**
-    * Zoom to a specific zoom level from user
-    * @param {number} zoom Zoom level.
-    * @api stable
-    */
-   M.impl.View.prototype.setUserZoom = function (zoom) {
-      this.userZoom_ = zoom;
-      this.setZoom(zoom);
-   };
+  /**
+   * Zoom to a specific zoom level from user
+   * @param {number} zoom Zoom level.
+   * @api stable
+   */
+  setUserZoom(zoom) {
+    this.userZoom_ = zoom;
+    this.setZoom(zoom);
+  }
 
-   /**
-    * Provides the zoom user
-    * @param {number} zoom Zoom level.
-    * @api stable
-    */
-   M.impl.View.prototype.getUserZoom = function (zoom) {
-      return this.userZoom_;
-   };
+  /**
+   * Provides the zoom user
+   * @param {number} zoom Zoom level.
+   * @api stable
+   */
+  getUserZoom(zoom) {
+    return this.userZoom_;
+  }
 
-   /**
-    * This function adds the control to the specified map
-    *
-    * @public
-    * @function
-    * @param {ol.proj.Projection} projection to apply to the map
-    * @api stable
-    */
-   M.impl.View.prototype.setProjection = function (projection) {
-      this.projection_ = projection;
-   };
+  /**
+   * This function adds the control to the specified map
+   *
+   * @public
+   * @function
+   * @param {ol.proj.Projection} projection to apply to the map
+   * @api stable
+   */
+  setProjection(projection) {
+    this.projection_ = projection;
+  }
 
-   /**
-    * This function provides the resolutions of the specified view
-    *
-    * @public
-    * @function
-    * @returns {Array<Number>} the resolutions
-    * @api stable
-    */
-   M.impl.View.prototype.getResolutions = function () {
-      return this.get('resolutions');
-   };
+  /**
+   * This function provides the resolutions of the specified view
+   *
+   * @public
+   * @function
+   * @returns {Array<Number>} the resolutions
+   * @api stable
+   */
+  getResolutions() {
+    return this.get('resolutions');
+  }
 
-   /**
-    * This function adds sets resolutions of the specified map
-    *
-    * @public
-    * @function
-    * @param {Array<Number>} resolutions to apply to the map
-    * @api stable
-    */
-   M.impl.View.prototype.setResolutions = function (resolutions) {
-      this.set('resolutions', resolutions);
-      this.maxResolution_ = resolutions[0];
-      this.minResolution_ = resolutions[resolutions.length - 1];
-      this.constraints_.resolution = ol.ResolutionConstraint.createSnapToResolutions(resolutions);
+  /**
+   * This function adds sets resolutions of the specified map
+   *
+   * @public
+   * @function
+   * @param {Array<Number>} resolutions to apply to the map
+   * @api stable
+   */
+  setResolutions(resolutions) {
+    this.set('resolutions', resolutions);
+    this.maxResolution_ = resolutions[0];
+    this.minResolution_ = resolutions[resolutions.length - 1];
+    this.constraints_.resolution = ol.ResolutionConstraint.createSnapToResolutions(resolutions);
 
-      // updates zoom
-      this.setZoom(this.userZoom_);
+    // updates zoom
+    this.setZoom(this.userZoom_);
 
-      // updates center
-      this.setCenter(this.getCenter());
-   };
-})();
+    // updates center
+    this.setCenter(this.getCenter());
+  }
+}
