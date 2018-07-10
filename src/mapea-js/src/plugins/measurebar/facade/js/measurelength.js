5a34964164bc01285438bb92491d1ad60793d6b9
carloscastellano <carloscastellano@carloscastellano.guadaltel.es>
2018-07-10 09:29:22 +0200

    Tarea #107390 Primera subida del plugin Measurebar de la migracion de modulos ES6.

--- src/mapea-js/src/plugins/measurebar/facade/js/measurelength.js
+++ src/mapea-js/src/plugins/measurebar/facade/js/measurelength.js
@@ -1,6 +1,7 @@
-goog.provide('P.control.MeasureLength');
-
-goog.require('P.control.Measure');
+import Measure from "./measurebase";
+import Utils from "facade/js/utils/utils";
+import Exception from "facade/js/exception/exception";
+import MeasureLenghtImpl from "../../impl/ol/js/measurelength";
 
 /**
  * @classdesc
@@ -11,37 +12,40 @@ goog.require('P.control.Measure');
  * @extends {M.control.Measure}
  * @api stable
  */
-M.control.MeasureLength = (function() {
-   // checks if the implementation can create WMC layers
-   if (M.utils.isUndefined(M.impl.control.MeasureLength)) {
-      M.exception('La implementación usada no puede crear controles MeasureLength');
-   }
 
-   // implementation of this control
-   var impl = new M.impl.control.MeasureLength();
+export default class MeasureLength extends Measure {
 
-   // calls the super constructor
-   goog.base(this, impl, M.control.MeasureLength.TEMPLATE);
-});
-goog.inherits(M.control.MeasureLength, M.control.Measure);
+  constructor() {
+    // implementation of this control
+    let impl = new MeasureLengthImpl();
 
-/**
- * This function checks if an object is equals
- * to this control
- *
- * @public
- * @function
- * @param {*} obj - Object to compare
- * @returns {boolean} equals - Returns if they are equal or not
- * @api stable
- */
-M.control.MeasureLength.prototype.equals = function(obj) {
-   var equals = false;
-   if (obj instanceof M.control.MeasureLength) {
+    // calls the super constructor
+    super(impl, MeasureLength.TEMPLATE);
+
+    // checks if the implementation can create WMC layers
+    if (Utils.isUndefined(MeasureLengthImpl)) {
+      Exception('La implementación usada no puede crear controles MeasureLength');
+    }
+  }
+
+  /**
+   * This function checks if an object is equals
+   * to this control
+   *
+   * @public
+   * @function
+   * @param {*} obj - Object to compare
+   * @returns {boolean} equals - Returns if they are equal or not
+   * @api stable
+   */
+  equals(obj) {
+    let equals = false;
+    if (obj instanceof MeasureLength) {
       equals = (this.name === obj.name);
-   }
-   return equals;
-};
+    }
+    return equals;
+  }
+}
 
 /**
  * Template for this controls
@@ -50,7 +54,7 @@ M.control.MeasureLength.prototype.equals = function(obj) {
  * @public
  * @api stable
  */
-M.control.MeasureLength.TEMPLATE = 'measurelength.html';
+MeasureLength.TEMPLATE = 'measurelength.html';
 
 /**
  * Help message
@@ -59,4 +63,4 @@ M.control.MeasureLength.TEMPLATE = 'measurelength.html';
  * @public
  * @api stable
  */
-M.control.MeasureLength.HELP_KEEP_MESSAGE = 'Click para continuar dibujando la línea';
+MeasureLength.HELP_KEEP_MESSAGE = 'Click para continuar dibujando la línea';
--- src/mapea-js/src/plugins/measurebar/impl/ol/js/measurelength.js
+++ src/mapea-js/src/plugins/measurebar/impl/ol/js/measurelength.js
@@ -1,7 +1,6 @@
-goog.provide('P.impl.control.MeasureLength');
-
-goog.require('P.impl.control.Measure');
-
+import Measure from "./measurebase";
+import FMeasureLength from "../../../js/measurelength";
+import FMeasureArea from "../../../js/measurearea";
 /**
  * @classdesc
  * Main constructor of the class. Creates a MeasureLength
@@ -11,54 +10,57 @@ goog.require('P.impl.control.Measure');
  * @extends {M.impl.control.Measure}
  * @api stable
  */
-M.impl.control.MeasureLength = function() {
-   /**
-    * Help message
-    * @private
-    * @type {string}
-    */
-   this.helpMsg_ = M.control.Measure.HELP_MESSAGE;
+export default class MeasureLength extends Measure {
 
-   /**
-    * Help message
-    * @private
-    * @type {string}
-    */
-   this.helpMsgContinue_ = M.control.MeasureLength.HELP_KEEP_MESSAGE;
+  constructor() {
 
-   goog.base(this, 'LineString');
-};
-goog.inherits(M.impl.control.MeasureLength, M.impl.control.Measure);
+    super('LineString');
+
+    /**
+     * Help message
+     * @private
+     * @type {string}
+     */
+    this.helpMsg_ = FMeasure.HELP_MESSAGE;
+
+    /**
+     * Help message
+     * @private
+     * @type {string}
+     */
+    this.helpMsgContinue_ = FMeasureLength.HELP_KEEP_MESSAGE;
+
+  }
 
 
-/**
- * This function add tooltip with measure distance
- * @public
- * @param {ol.geom.SimpleGeometry} geometry - Object geometry
- * @return {string} output - Indicates the measure distance
- * @api stable
- */
-M.impl.control.MeasureLength.prototype.formatGeometry = function(geometry) {
-   var length = Math.round(geometry.getLength() * 100) / 100;
-   var output;
-   if (length > 100) {
+  /**
+   * This function add tooltip with measure distance
+   * @public
+   * @param {ol.geom.SimpleGeometry} geometry - Object geometry
+   * @return {string} output - Indicates the measure distance
+   * @api stable
+   */
+  formatGeometry(geometry) {
+    let length = Math.round(geometry.getLength() * 100) / 100;
+    let output;
+    if (length > 100) {
       output = (Math.round(length / 1000 * 100) / 100) +
-         ' ' + 'km';
-   }
-   else {
+        ' ' + 'km';
+    } else {
       output = (Math.round(length * 100) / 100) +
-         ' ' + 'm';
-   }
-   return output;
-};
+        ' ' + 'm';
+    }
+    return output;
+  }
 
-/**
- * This function returns coordinates to tooltip
- * @public
- * @param {ol.geom.SimpleGeometry} geometry - Object geometry
- * @return {array} coordinates to tooltip
- * @api stable
- */
-M.impl.control.MeasureLength.prototype.getTooltipCoordinate = function(geometry) {
-   return geometry.getLastCoordinate();
-};
+  /**
+   * This function returns coordinates to tooltip
+   * @public
+   * @param {ol.geom.SimpleGeometry} geometry - Object geometry
+   * @return {array} coordinates to tooltip
+   * @api stable
+   */
+  getTooltipCoordinate(geometry) {
+    return geometry.getLastCoordinate();
+  }
+}
