a4fde1139f7c44f9885f2cf77d527791a83d5340
emrivero <emiliomartinez@guadaltel.com>
2018-07-11 08:52:37 +0200

    Peerreview #106854 #107042
    Se suben las correcciones de la clase facade/dialog vistas en la peerreview 20180711

--- src/mapea-js/src/facade/js/dialog.js
+++ src/mapea-js/src/facade/js/dialog.js
@@ -1,101 +1,95 @@
-import Utils from "./utils/utils";
-import Template from "./utils/template";
+import Utils from "./util/Utils";
+import Template from "./util/Template";
+import dialogTemplate from "templates/dialog.html";
 
-export default class Dialog {
-  /**
-   * TODO
-   *
-   * @public
-   * @function
-   * @api stable
-   * @returns {Promise}
-   */
-  static show(message, title, severity) {
-    return Template.compile('dialog.html', {
-      'jsonp': true,
-      'vars': {
-        'message': message,
-        'title': title,
-        'severity': severity
-      }
-    }).then((html) => {
-      // removes previous dialogs
-      Dialog.remove();
+/**
+ * TODO
+ *
+ * @public
+ * @function
+ * @api stable
+ * @returns {Promise}
+ */
+export const show = (message, title, severity) => {
+  let vars = {
+    message,
+    title,
+    severity
+  };
+  let html = Template.compile(dialogTemplate, {
+    vars
+  });
+  // removes previous dialogs
+  remove();
 
-      // append new dialog
-      let mapeaContainer = document.querySelector('div.m-mapea-container');
+  // append new dialog
+  let mapeaContainer = document.querySelector('div.m-mapea-container');
 
-      // adds listener to close the dialog
-      let okButton = html.querySelector('div.m-button > button');
-      okButton.addEventListener("click", evt => {
-        okButton.removeNode(html);
-      });
-      mapeaContainer.appendChild(html);
-    });
+  // adds listener to close the dialog
+  let okButton = html.querySelector('div.m-button > button');
+  okButton.addEventListener("click", evt => okButton.removeNode(html));
+  mapeaContainer.appendChild(html);
+};
+
+/**
+ * TODO
+ *
+ * @public
+ * @function
+ * @api stable
+ */
+export const remove = () => {
+  let dialogs = document.querySelectorAll('div.m-dialog');
+  Array.prototype.forEach.call(dialogs, dialog => dialogs.removeNode(dialog));
+};
+
+/**
+ * TODO
+ *
+ * @public
+ * @function
+ * @param {string} message to show
+ * @param {string} title of the dialog
+ * @api stable
+ * @returns {Promise}
+ */
+export const info = (message, title) => {
+  if (Utils.isNullOrEmpty(title)) {
+    title = 'INFORMACIÓN';
   }
+  return show(message, title, 'info');
+};
 
-  /**
-   * TODO
-   *
-   * @public
-   * @function
-   * @api stable
-   */
-  static remove() {
-    let dialogs = document.querySelectorAll('div.m-dialog');
-    Array.prototype.forEach.call(dialogs, dialog => {
-      dialogs.removeNode(dialog);
-    });
+/**
+ * TODO
+ *
+ * @public
+ * @function
+ * @param {string} message to show
+ * @param {string} title of the dialog
+ * @api stable
+ * @returns {Promise}
+ */
+export const error = (message, title) => {
+  if (Utils.isNullOrEmpty(title)) {
+    title = 'ERROR';
   }
+  return show(message, title, 'error');
+};
 
-  /**
-   * TODO
-   *
-   * @public
-   * @function
-   * @param {string} message to show
-   * @param {string} title of the dialog
-   * @api stable
-   * @returns {Promise}
-   */
-  static info(message, title) {
-    if (Utils.isNullOrEmpty(title)) {
-      title = 'INFORMACIÓN';
-    }
-    return Dialog.show(message, title, 'info');
+/**
+ * TODO
+ *
+ * @public
+ * @function
+ * @param {string} message to show
+ * @param {string} title of the dialog
+ * @api stable
+ * @returns {Promise}
+ */
+export const success = (message, title) => {
+  if (Utils.isNullOrEmpty(title)) {
+    title = 'ÉXITO';
   }
-
-  /**
-   * TODO
-   *
-   * @public
-   * @function
-   * @param {string} message to show
-   * @param {string} title of the dialog
-   * @api stable
-   * @returns {Promise}
-   */
-  static error(message, title) {
-    if (Utils.isNullOrEmpty(title)) {
-      title = 'ERROR';
-    }
-    return Dialog.show(message, title, 'error');
-  }
-
-  /**
-   * TODO
-   *
-   * @public
-   * @function
-   * @param {string} message to show
-   * @param {string} title of the dialog
-   * @api stable
-   * @returns {Promise}
-   */
-  static success(message, title) {
-    if (Utils.isNullOrEmpty(title)) {
-      title = 'ÉXITO';
-    }
-    return Dialog.show(message, title, 'success');
-  }
-}
+  return show(message, title, 'success');
+};
