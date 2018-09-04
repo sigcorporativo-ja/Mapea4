f1f195d7b2cf925cae84e0627447df37142668e3
carloscastellano <carloscastellano@carloscastellano.guadaltel.es>
2018-09-04 14:30:05 +0200

    Incidencia #110100
    Se corrige error respecto al "filtrado espacial" , además soluciona varios errores a la hora de utilizar "jsts".

--- src/mapea-js/src/impl/ol/js/format/WKT.js
+++ src/mapea-js/src/impl/ol/js/format/WKT.js
@@ -20,13 +20,12 @@ export default class WKT extends OLFormatWKT {
    * @inheritDoc
    */
   write(geometry) {
-    let wktGeom;
-    let olGeometry = this.gjFormat_.readGeometryFromObject(geometry);
-    if (olGeometry.getType().toLowerCase() === "point") {
-      let pointCoord = olGeometry.getCoordinates();
+    const olGeometry = this.gjFormat_.readGeometryFromObject(geometry);
+    if (olGeometry.getType().toLowerCase() === 'point') {
+      const pointCoord = olGeometry.getCoordinates();
       olGeometry.setCoordinates([pointCoord[0], pointCoord[1]]);
     }
-    wktGeom = this.writeGeometryText(olGeometry);
+    const wktGeom = this.writeGeometryText(olGeometry);
     return wktGeom;
   }
 }
