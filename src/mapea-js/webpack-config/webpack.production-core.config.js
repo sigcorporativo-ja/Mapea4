664a895ee96b52307b5186defee1670cc6374623
emrivero <emiliomartinez@guadaltel.com>
2018-09-06 14:07:45 +0200

    Mejora de formato de webpack-config

--- src/mapea-js/webpack-config/webpack.production-core.config.js
+++ src/mapea-js/webpack-config/webpack.production-core.config.js
@@ -1,28 +1,32 @@
 const path = require('path');
-const webpack = require('webpack');
 const AllowMutateEsmExports = require('./AllowMutateEsmExportsPlugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopywebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const argv = require('yargs').argv;
+
+const sourcemap = argv['source-map'];
+const pathsToClean = [
+  path.resolve(__dirname, '..', 'dist', 'js'),
+];
 
 module.exports = {
   mode: 'production',
   entry: {
-    'mapea.ol.min': path.resolve(__dirname, '../src/index.js')
+    'mapea.ol.min': path.resolve(__dirname, '..', 'src', 'index.js'),
   },
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, '..', 'dist'),
     filename: 'js/[name].js',
   },
   resolve: {
     alias: {
       handlebars: 'handlebars/dist/handlebars.min.js',
-      templates: path.resolve(__dirname, '../src/templates'),
-      assets: path.resolve(__dirname, '../src/facade/assets'),
-      facade: path.resolve(__dirname, '../src/facade'),
-      impl: path.resolve(__dirname, '../src/impl/ol/js'),
-      // configuration: path.resolve(__dirname, '../test/configuration_filtered'),
-      'impl-assets': path.resolve(__dirname, '../src/impl/ol/assets'),
-      // plugins: path.resolve(__dirname, '../src/plugins'),
+      templates: path.resolve(__dirname, '..', 'src', 'templates'),
+      assets: path.resolve(__dirname, '..', 'src', 'facade', 'assets'),
+      facade: path.resolve(__dirname, '..', 'src', 'facade'),
+      impl: path.resolve(__dirname, '..', 'src', 'impl', 'ol', 'js'),
+      'impl-assets': path.resolve(__dirname, '..', 'src', 'impl', 'ol', 'assets'),
     },
     extensions: ['.wasm', '.mjs', '.js', '.json', '.css', '.hbs', '.html'],
   },
@@ -65,13 +69,17 @@ module.exports = {
       }],
   },
   plugins: [
+    new CleanWebpackPlugin(pathsToClean, {
+      root: path.resolve(__dirname, '..'),
+    }),
     new AllowMutateEsmExports(),
     new MiniCssExtractPlugin({
       filename: 'assets/css/[name].css',
     }),
     new CopywebpackPlugin([{
       from: 'src/configuration.js',
-      to: 'js/configuration.js'
+      to: 'js/configuration.js',
     }]),
   ],
+  devtool: sourcemap ? 'source-map' : '',
 };
--- src/mapea-js/webpack-config/webpack.production-plugins.config.js
+++ src/mapea-js/webpack-config/webpack.production-plugins.config.js
@@ -1,12 +1,10 @@
 const path = require('path');
-const webpack = require('webpack');
-const AllowMutateEsmExports = require('./AllowMutateEsmExportsPlugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopywebpackPlugin = require('copy-webpack-plugin');
 const entryPoints = require('./entry-points-plugins.json');
 
-const distDir = path.resolve(__dirname, '../dist');
-const pluginDir = path.resolve(__dirname, '../src/plugins');
+const distDir = path.resolve(__dirname, '..', 'dist');
+const pluginDir = path.resolve(__dirname, '..', 'src', 'plugins');
 
 module.exports = {
   mode: 'production',
@@ -17,13 +15,6 @@ module.exports = {
   },
   resolve: {
     alias: {
-      handlebars: 'handlebars/dist/handlebars.min.js',
-      templates: path.resolve(__dirname, '../src/templates'),
-      assets: path.resolve(__dirname, '../src/facade/assets'),
-      facade: path.resolve(__dirname, '../src/facade'),
-      impl: path.resolve(__dirname, '../src/impl/ol/js'),
-      configuration: path.resolve(__dirname, '../test/configuration_filtered'),
-      'impl-assets': path.resolve(__dirname, '../src/impl/ol/assets'),
       plugins: pluginDir,
     },
     extensions: ['.wasm', '.mjs', '.js', '.json', '.css', '.hbs', '.html'],
@@ -67,7 +58,6 @@ module.exports = {
       }],
   },
   plugins: [
-    new AllowMutateEsmExports(),
     new CopywebpackPlugin([{
       from: path.join(pluginDir, '**', 'api.json'),
       to: path.join(distDir, 'plugins/[1]/api.json'),
