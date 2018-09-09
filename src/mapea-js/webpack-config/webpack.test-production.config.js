dcf842d74098653a6f3750d4e3049ca8f0169733
Emilio Martinez Rivero <asemeu7@gmail.com>
2018-09-09 20:26:49 +0200

    Se mejora configuracion de webpack
    Al igual que en la configuracion de desarrollo, en la configuracion de testeo de produccion se parametriza el nombre del test a desplegar.

--- src/mapea-js/webpack-config/webpack.test-production.config.js
+++ src/mapea-js/webpack-config/webpack.test-production.config.js
@@ -1,38 +1,45 @@
 const path = require('path');
 const webpack = require('webpack');
+const fse = require('fs-extra');
+const argv = require('yargs').argv;
 
-const distPath = path.resolve(__dirname, '../dist/js/mapea.ol.min.js');
-const testPreprod = path.resolve(__dirname, '../test/production/basic-map.js');
+const testName = argv.name;
+if (testName === undefined) {
+  const error = new Error('Test name is undefined. Use: npm debug:build -- --name=<test-name>');
+  throw error;
+}
+const testPath = path.resolve(__dirname, '..', 'test', 'production', `${testName}.js`);
+const testHTMLPath = path.resolve(__dirname, '..', 'test', 'production', `${testName}.html`);
+
+try {
+  fse.statSync(testPath).isFile();
+} catch (e) {
+  const error = new Error('Javascript test does not exist. Be sure to write the test name correctly.');
+  throw error;
+}
+
+try {
+  fse.statSync(testHTMLPath).isFile();
+} catch (e) {
+  const error = new Error('HTML test does not exist. Be sure to name the html test as the js test.');
+  throw error;
+}
+
 const plugins = [new webpack.HotModuleReplacementPlugin()];
 
 module.exports = {
   mode: 'development',
   entry: {
-    'test-preprod': testPreprod,
-    'mapea.ol.min': distPath,
+    'test-preprod': testPath,
   },
   output: {
-    path: distPath,
     filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules\/(?!ol)|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      }],
   },
   plugins: [...plugins],
   devServer: {
     hot: true,
     open: true,
-    openPage: 'test/production/basic-map.html',
+    openPage: `test/production/${testName}.html`,
     watchOptions: {
       poll: 1000,
     },
