e5ef0207f06d56332859223c54765c3c672e55a8
emrivero <emiliomartinez@guadaltel.com>
2019-02-06 14:18:39 +0100

    Issue #232
    Se muestra una leyenda informativa cuando los estilos son dinamicos.

--- mapea-js/src/impl/ol/js/style/Point.js
+++ mapea-js/src/impl/ol/js/style/Point.js
@@ -1,7 +1,7 @@
 /**
  * @module M/impl/style/Point
  */
-import { isNullOrEmpty, concatUrlPaths, addParameters } from 'M/util/Utils';
+import { isNullOrEmpty, concatUrlPaths, addParameters, isDynamic, drawDynamicStyle } from 'M/util/Utils';
 import chroma from 'chroma-js';
 import OLStyleImage from 'ol/style/Image';
 import OLFeature from 'ol/Feature';
@@ -37,46 +37,50 @@ class Point extends Simple {
    * @return {String} data url to canvas
    * @api stable
    */
-  toImage() {
-    if (isNullOrEmpty(this.olStyleFn_)) {
-      return null;
-    }
-    let style = this.olStyleFn_()[1];
+  toImage(canvas) {
     let image = null;
-    if (style.getImage && style.getImage() != null && style.getImage() instanceof OLStyleImage) {
-      // see https://github.com/openlayers/openlayers/blob/master/src/ol/style/regularshape.js#L205
-      if (style.getImage() instanceof PointFontSymbol) {
-        const imageCanvas = style.getImage().getImage();
-        if (imageCanvas != null && imageCanvas) {
-          image = imageCanvas.toDataURL();
-        }
-      } else if (style.getImage() instanceof PointIcon) {
-        const imageStyle = style.getImage();
-        // let canvasSize = this.getCanvasSize();
-        // canvasSize[0] / size[0]) * size[0]
-        // let [size, scale] = [imageStyle.getSize(), imageStyle.getScale()];
-        // ctx.drawImage(imageStyle.getImage(), 0, 0, ctx.canvas.height, ctx.canvas.width);
-        if (!isNullOrEmpty(imageStyle)) {
-          image = imageStyle.getSrc();
-          if (!image.startsWith(window.location.origin)) {
-            const proxyImageURL = concatUrlPaths([M.config.PROXY_URL, '/image']);
-            image = addParameters(proxyImageURL, {
-              url: image,
-            });
+    if (isDynamic(this.options_) === true) {
+      image = drawDynamicStyle(canvas);
+    } else {
+      if (isNullOrEmpty(this.olStyleFn_)) {
+        return null;
+      }
+      let style = this.olStyleFn_()[1];
+      if (style.getImage && style.getImage() != null && style.getImage() instanceof OLStyleImage) {
+        // see https://github.com/openlayers/openlayers/blob/master/src/ol/style/regularshape.js#L205
+        if (style.getImage() instanceof PointFontSymbol) {
+          const imageCanvas = style.getImage().getImage();
+          if (imageCanvas != null && imageCanvas) {
+            image = imageCanvas.toDataURL();
+          }
+        } else if (style.getImage() instanceof PointIcon) {
+          const imageStyle = style.getImage();
+          // let canvasSize = this.getCanvasSize();
+          // canvasSize[0] / size[0]) * size[0]
+          // let [size, scale] = [imageStyle.getSize(), imageStyle.getScale()];
+          // ctx.drawImage(imageStyle.getImage(), 0, 0, ctx.canvas.height, ctx.canvas.width);
+          if (!isNullOrEmpty(imageStyle)) {
+            image = imageStyle.getSrc();
+            if (!image.startsWith(window.location.origin)) {
+              const proxyImageURL = concatUrlPaths([M.config.PROXY_URL, '/image']);
+              image = addParameters(proxyImageURL, {
+                url: image,
+              });
+            }
           }
         }
-      }
-    } else {
-      style = this.olStyleFn_()[0];
-      if (style.getImage() != null && style.getImage().getStroke() != null) {
-        if (style.getImage().getStroke().getWidth() > Point.DEFAULT_WIDTH_POINT) {
-          style.getImage().getStroke().setWidth(Point.DEFAULT_WIDTH_POINT);
+      } else {
+        style = this.olStyleFn_()[0];
+        if (style.getImage() != null && style.getImage().getStroke() != null) {
+          if (style.getImage().getStroke().getWidth() > Point.DEFAULT_WIDTH_POINT) {
+            style.getImage().getStroke().setWidth(Point.DEFAULT_WIDTH_POINT);
+          }
+          style.getImage().render();
         }
-        style.getImage().render();
-      }
-      const imageCanvas = style.getImage().getImage();
-      if (imageCanvas != null) {
-        image = imageCanvas.toDataURL();
+        const imageCanvas = style.getImage().getImage();
+        if (imageCanvas != null) {
+          image = imageCanvas.toDataURL();
+        }
       }
     }
     return image;
@@ -131,12 +135,12 @@ class Point extends Simple {
         const strokeColorValue =
           Simple.getValue(options.stroke.color, featureVariable, this.layer_);
         if (!isNullOrEmpty(strokeColorValue)) {
+          const { linedashoffset } = options.stroke;
           stroke = new OLStyleStroke({
             color: strokeColorValue,
             width: Simple.getValue(options.stroke.width, featureVariable, this.layer_),
             lineDash: Simple.getValue(options.stroke.linedash, featureVariable, this.layer_),
-            lineDashOffset: Simple
-              .getValue(options.stroke.linedashoffset, featureVariable, this.layer_),
+            lineDashOffset: Simple.getValue(linedashoffset, featureVariable, this.layer_),
             lineCap: Simple.getValue(options.stroke.linecap, featureVariable, this.layer_),
             lineJoin: Simple.getValue(options.stroke.linejoin, featureVariable, this.layer_),
             miterLimit: Simple.getValue(options.stroke.miterlimit, featureVariable, this.layer_),
@@ -164,16 +168,15 @@ class Point extends Simple {
           rotation: Simple.getValue(options.label.rotation, featureVariable, this.layer_),
         });
         if (!isNullOrEmpty(options.label.stroke)) {
+          const { miterlimit, linedashoffset } = this.options.label.stroke;
           labelText.setStroke(new OLStyleStroke({
             color: Simple.getValue(options.label.stroke.color, featureVariable, this.layer_),
             width: Simple.getValue(options.label.stroke.width, featureVariable, this.layer_),
             lineCap: Simple.getValue(options.label.stroke.linecap, featureVariable, this.layer_),
             lineJoin: Simple.getValue(options.label.stroke.linejoin, featureVariable, this.layer_),
             lineDash: Simple.getValue(options.label.stroke.linedash, featureVariable, this.layer_),
-            lineDashOffset: Simple
-              .getValue(options.label.stroke.linedashoffset, featureVariable, this.layer_),
-            miterLimit: Simple
-              .getValue(options.label.stroke.miterlimit, featureVariable, this.layer_),
+            lineDashOffset: Simple.getValue(linedashoffset, featureVariable, this.layer_),
+            miterLimit: Simple.getValue(miterlimit, featureVariable, this.layer_),
           }));
         }
         style.setText(labelText);
@@ -330,3 +333,4 @@ class Point extends Simple {
 Point.DEFAULT_WIDTH_POINT = 3;
 
 export default Point;
+
