c4f41a87737110b7ab68ede1c477975d3b9afe3a
Emilio Martinez Rivero <emiliomartinezriv@gmail.com>
2019-10-11 14:15:50 +0200

    Tarea #130503
    Se añade al core el plugin de GeosearchByCoordinates

---
+++ mapea-js/src/plugins/geosearchbycoordinates/impl/ol/js/geosearch/utils.js
@@ -0,0 +1,46 @@
+export default class UtilsGeosearch {
+  static getCentroidCoordinate(geometry) {
+    let centroid;
+    let coordinates;
+    let medianIdx;
+    let points;
+    let lineStrings;
+    let geometries;
+
+    // POINT
+    if (geometry.getType() === M.geom.wkt.type.POINT) {
+      centroid = geometry.getCoordinates();
+    } else if (geometry.getType() === M.geom.wkt.type.LINE_STRING) {
+      // LINE
+      coordinates = geometry.getCoordinates();
+      medianIdx = Math.floor(coordinates.length / 2);
+      centroid = coordinates[medianIdx];
+    } else if (geometry.getType() === M.geom.wkt.type.LINEAR_RING) {
+      coordinates = geometry.getCoordinates();
+      medianIdx = Math.floor(coordinates.length / 2);
+      centroid = coordinates[medianIdx];
+    } else if (geometry.getType() === M.geom.wkt.type.POLYGON) {
+      // POLYGON
+      centroid = UtilsGeosearch.getCentroidCoordinate(geometry.getInteriorPoint());
+    } else if (geometry.getType() === M.geom.wkt.type.MULTI_POINT) {
+      // MULTI
+      points = geometry.getPoints();
+      medianIdx = Math.floor(points.length / 2);
+      centroid = UtilsGeosearch.getCentroidCoordinate(points[medianIdx]);
+    } else if (geometry.getType() === M.geom.wkt.type.MULTI_LINE_STRING) {
+      lineStrings = geometry.getLineStrings();
+      medianIdx = Math.floor(lineStrings.length / 2);
+      centroid = UtilsGeosearch.getCentroidCoordinate(lineStrings[medianIdx]);
+    } else if (geometry.getType() === M.geom.wkt.type.MULTI_POLYGON) {
+      points = geometry.getInteriorPoints();
+      centroid = UtilsGeosearch.getCentroidCoordinate(points);
+    } else if (geometry.getType() === M.geom.wkt.type.CIRCLE) {
+      centroid = geometry.getCenter();
+    } else if (geometry.getType() === M.geom.wkt.type.GEOMETRY_COLLECTION) {
+      geometries = geometry.getGeometries();
+      medianIdx = Math.floor(geometries.length / 2);
+      centroid = UtilsGeosearch.getCentroidCoordinate(geometries[medianIdx]);
+    }
+    return centroid;
+  }
+}
