811de59cda3cd8702ad487ae0ca2632dac5eb43e
Carmen <carmenmarquez@guadaltel.com>
2022-07-19 08:28:54 +0200

    Tarea #236426: se elimina de la rama principal el codigo

--- mapea-js/src/facade/js/layer/WMS.js
+++ mapea-js/src/facade/js/layer/WMS.js
@@ -153,6 +153,8 @@ class WMS extends LayerBase {
   set version(newVersion) {
     if (!isNullOrEmpty(newVersion)) {
       this.getImpl().version = newVersion;
+    } else {
+      this.getImpl().version = '1.1.0'; // default value
     }
   }
 
