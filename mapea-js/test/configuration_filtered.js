e32421f4eae51b9fffc1849bd0bb8fae1f142f9f
Carmen <carmenmarquez@guadaltel.com>
2022-07-27 09:16:24 +0200

    Cambio properties para despliegue con nueva version de OL

--- mapea-js/test/configuration_filtered.js
+++ mapea-js/test/configuration_filtered.js
@@ -24,7 +24,7 @@ function fun(M_) {
    * @public
    * @api stable
    */
-  M_.config('MAPEA_URL', location.protocol + '//sigc-dev.desarrollo.guadaltel.es/mapea6');
+  M_.config('MAPEA_URL', location.protocol + '//sigc-dev-ol.desarrollo.guadaltel.es/mapea6');
 
   /**
    * The path to the Mapea proxy to send
@@ -34,7 +34,7 @@ function fun(M_) {
    * @public
    * @api stable
    */
-  M_.config('PROXY_URL', location.protocol + '//sigc-dev.desarrollo.guadaltel.es/mapea6/api/proxy');
+  M_.config('PROXY_URL', location.protocol + '//sigc-dev-ol.desarrollo.guadaltel.es/mapea6/api/proxy');
 
   /**
    * The path to the Mapea proxy to send
@@ -44,7 +44,7 @@ function fun(M_) {
    * @public
    * @api stable
    */
-  M_.config('PROXY_POST_URL', location.protocol + '//sigc-dev.desarrollo.guadaltel.es/mapea6/proxyPost');
+  M_.config('PROXY_POST_URL', location.protocol + '//sigc-dev-ol.desarrollo.guadaltel.es/mapea6/proxyPost');
 
   /**
    * The path to the Mapea templates
