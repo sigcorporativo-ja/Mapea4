2f9d16e0ba51ec4f9386b646ed6a35c9e9e98db2
Antonio Manuel Rebollar Marquez <antoniomrebollar@guadaltel.es>
2023-07-10 09:55:17 +0200

    Anadido soporte para tickets en el plugin wfstcontrols

--- mapea-js/src/plugins/wfstcontrols/impl/ol/js/savefeature.js
+++ mapea-js/src/plugins/wfstcontrols/impl/ol/js/savefeature.js
@@ -102,7 +102,7 @@ export default class SaveFeature extends M.impl.Control {
       const wfstRequestText = oSerializer.serializeToString(wfstRequestXml);
 
       // const wfstRequestText = goog.dom.xml.serialize(wfstRequestXml);
-      M.remote.post(this.layer_.url, wfstRequestText).then((response) => {
+      M.remote.post(`${this.layer_.url}&ticket=${window.mapjs.getTicket()}`, wfstRequestText).then((response) => {
         // clears layer
         const clearCtrl = this.facadeMap_.getControls(FClearFeature.NAME)[0];
         clearCtrl.getImpl().clear();
