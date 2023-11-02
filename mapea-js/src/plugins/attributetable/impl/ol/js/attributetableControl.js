f47eab6d97b2c893773131ffdab19ca172abc78f
Antonio Manuel Rebollar Marquez <antoniomrebollar@guadaltel.com>
2023-11-02 11:53:04 +0100

    plugin attributetable v2.2.0

--- mapea-js/src/plugins/attributetable/impl/ol/js/attributetableControl.js
+++ mapea-js/src/plugins/attributetable/impl/ol/js/attributetableControl.js
@@ -24,6 +24,51 @@ export default class AttributeTableControlImpl extends M.impl.Control {
   }
 
   /**
+   * Transforms x,y coordinates to 4326 on coordinates array.
+   * @public
+   * @function
+   * @api
+   * @param {String} codeProjection
+   * @param {Array<Number>} oldCoordinates
+   */
+  getTransformedCoordinates(codeProjection, oldCoordinates) {
+    const transformFunction = ol.proj.getTransform(codeProjection, 'EPSG:4326');
+    return this.getFullCoordinates(
+      oldCoordinates,
+      transformFunction(this.getXY(oldCoordinates)),
+    );
+  }
+
+  /**
+   * Substitutes x, y coordinates on coordinate set (x, y, altitude...)
+   * @public
+   * @function
+   * @api
+   * @param {Array} oldCoordinates
+   * @param {Array<Number>} newXY - [x,y]
+   */
+  getFullCoordinates(oldCoordinates, newXY) {
+    const newCoordinates = oldCoordinates;
+    newCoordinates[0] = newXY[0];
+    newCoordinates[1] = newXY[1];
+    return newCoordinates;
+  }
+
+  /**
+   * Given a coordinate set (x, y, altitude?), returns [x,y].
+   * @public
+   * @function
+   * @api
+   * @param {Array<Number>} coordinatesSet
+   */
+  getXY(coordinatesSet) {
+    const coordinateCopy = [];
+    for (let i = 0; i < coordinatesSet.length; i += 1) coordinateCopy.push(coordinatesSet[i]);
+    while (coordinateCopy.length > 2) coordinateCopy.pop();
+    return coordinateCopy;
+  }
+
+  /**
    * LayerSwitcher panel id
    * @const
    * @type {string}
